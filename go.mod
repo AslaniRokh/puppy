7695ec7dc5300822c97eaf785c30c4a9a2a7228a
ASLANIROKH <behnamaslani@proton.me>
2025-07-11 19:48:42 +0330

    i don't know the number of commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AslaniRokh/puppy
 
 go 1.24.2
+
+require github.com/AslaniRokh/dog v0.0.0-20250711161128-31a08baff5b6 // indirect
--- puppyali.go
+++ puppyali.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/AslaniRokh/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barcks() string {
 	return "woof woof woof!"
+}
+
+func BigBarck() string {
+	return dog.BigDog(Bark())
+}
+
+func BigBarcks() string {
+	return dog.BigDog(Barcks())
 }
